411bf9fa034e36e153028dcdff5df365f4ba72d4
IgnacioPM <65568535+IgnacioPM@users.noreply.github.com>
2021-06-21 22:02:58 -0600

    preferences perfil

--- lib/screens/perfil_page.dart
+++ lib/screens/perfil_page.dart
@@ -3,7 +3,6 @@ import 'package:runningup/constants/Theme.dart';
 import 'package:runningup/widgets/drawer.dart';
 import 'package:runningup/prefences/user_preference.dart';
 
-
 // import 'package:runningup/widgets/navbar.dart';
 class PerfilPage extends StatefulWidget {
   static String id = 'Perfil_Page';
@@ -13,41 +12,31 @@ class PerfilPage extends StatefulWidget {
 }
 
 class _PerfilPageState extends State<PerfilPage> {
-  
+  UserPreference userPreference = UserPreference();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           centerTitle: true,
           title: Text('Mi Perfil'),
         ),
         backgroundColor: MaterialColors.bgColorScreen,
-        drawer: MaterialDrawer(currentPage:"Perfil_Page"),
+        drawer: MaterialDrawer(currentPage: "Perfil_Page"),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-
             children: [
-            SizedBox(
+              SizedBox(
                 height: 15.0,
               ),
-            Text(
-              'Hola este es tu perfil papu ^_~'
-            ),
-            
-            SizedBox(
-              height: 0.5,
-            ),
-          ],
-
+              Text('Hola este es tu perfil papu ^_~'),
+              SizedBox(
+                height: 0.5,
+              ),
+            ],
           ),
-          
-        ) 
-        );
-      
-  
+        ));
   }
-
-
 }
