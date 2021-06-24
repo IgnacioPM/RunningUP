729af8d2aa9c8e04b11ba0bf426df5df86a6dc0a
IgnacioPM <65568535+IgnacioPM@users.noreply.github.com>
2021-06-23 23:32:39 -0600

    on tap

--- lib/screens/compas_page.dart
+++ lib/screens/compas_page.dart
@@ -52,6 +52,9 @@ class _CompasPageState extends State<CompasPage> {
                 itemBuilder: (BuildContext context, index) {
                   Compas users = snapshot.data[index];
                   return ListTile(
+                    onTap: () {
+                      Navigator.pushReplacementNamed(context, '/Perfil');
+                    },
                     leading: CircleAvatar(
                       backgroundImage: NetworkImage(
                           "https://images.unsplash.com/photo-1553544923-37efbe6ff816?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=368&q=80"),
