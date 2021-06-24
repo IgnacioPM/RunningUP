aad7494bfea9115ffc1e26bc82c5da414490f896
Oscar A. Mora Villalobos <oscar.mora.villalobos@est.una.ac.cr>
2021-06-24 14:52:08 -0600

    permisos de camara

--- lib/screens/RegistroAct_page.dart
+++ lib/screens/RegistroAct_page.dart
@@ -4,7 +4,8 @@ import 'package:runningup/widgets/drawer.dart';
 
 // import 'package:runningup/widgets/navbar.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
-
+import 'package:permission_handler/permission_handler.dart';
+import 'package:oktoast/oktoast.dart';
 // import 'package:stop_watch_timer/stop_watch_timer.dart';
 class RegisActPage extends StatefulWidget {
   static String id = 'RegisAct_Page';
@@ -16,6 +17,27 @@ class RegisActPage extends StatefulWidget {
 class _RegisActPageState extends State<RegisActPage> {
   String qrValue = "Codigo Qr";
   String msj = '';
+  verificarpermisos_camara() async{
+        var estatusCamara = await Permission.camera.status;
+    var almacen = await Permission.storage.status;
+    print(estatusCamara);
+    print(almacen);
+  //cameraStatus.isGranted == has access to application
+    //cameraStatus.isDenied == does not have access to application, you can request again for the permission. 
+    //cameraStatus.isPermanentlyDenied == does not have access to application, you cannot request again for the permission. 
+    //cameraStatus.isRestricted == because of security/parental control you cannot use this permission. 
+    //cameraStatus.isUndetermined == permission has not asked before. 
+    if(!estatusCamara.isGranted)
+      await Permission.camera.request();
+    if(await Permission.camera.isGranted){
+      scanQr();
+    }else{
+      showToast("Para leer el codigo QR por favor habilita el permiso del uso de camara", position: ToastPosition.bottom);
+    }
+    
+  }
+
+
 
   void scanQr() async {
     String cameraScanResult = await scanner.scan();
@@ -90,7 +112,7 @@ class _RegisActPageState extends State<RegisActPage> {
         SizedBox(width: 30),
         FloatingActionButton(
           backgroundColor: Colors.green[900],
-          onPressed: () => scanQr(),
+          onPressed: () => verificarpermisos_camara(),
           child: Icon(
             Icons.camera_alt_outlined,
           ),
--- lib/screens/crono_page.dart
+++ lib/screens/crono_page.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 // import 'package:runningup/constants/Theme.dart';
 // import 'package:runningup/widgets/drawer.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:qrscan/qrscan.dart' as scanner;
 
 import 'estadisticas_page.dart';
@@ -14,6 +16,9 @@ class CronoPage extends StatefulWidget {
 }
 
 class _CronoPageState extends State<CronoPage> {
+  TextEditingController controllerTiempo = new TextEditingController();
+  TextEditingController controllerFecha = new TextEditingController();
+
   void initState() {
     super.initState();
     WidgetsBinding.instance
@@ -25,21 +30,37 @@ class _CronoPageState extends State<CronoPage> {
   final _stopWatch = new Stopwatch();
   final _timeout = const Duration(milliseconds: 1);
   String qrValue = "Codigo Qr";
-  String xd = '';
+  String xd = 'hola';
   String msj = '';
+  String fecha = 'dfd';
+  int id = 1;
+  
+
+void addData(){
+controllerTiempo.text = xd;
+controllerFecha.text = fecha;
+
+    var url = Uri.parse("https://runningup.000webhostapp.com/Addtiempo_reco.php");
+     http.post(url, body: {
+        "tiempo": controllerTiempo,
+        "fecha": controllerFecha,
+      });
+}
 
   void scanQr() async {
     String cameraScanResult = await scanner.scan();
+
     setState(() {
       qrValue = cameraScanResult;
     });
+
     // if (cameraScanResult.length > 0) {
     //         Navigator.pushReplacementNamed(context, '/Crono');
     //       }
     if (qrValue == 'Finalizar') {
       _stopWatch.stop();
       xd = _stopwatchText;
-      // Navigator.pushReplacementNamed(context, '/Estadisticas');
+      addData();
       Navigator.push(context,
           MaterialPageRoute(builder: (context) => EstadisticaPage(xd)));
     } else {
@@ -75,15 +96,15 @@ class _CronoPageState extends State<CronoPage> {
     });
   }
 
-  void _resetButtonPressed() {
-    if (_stopWatch.isRunning) {
-      _startStopButtonPressed();
-    }
-    setState(() {
-      _stopWatch.reset();
-      _setStopwatchText();
-    });
-  }
+  // void _resetButtonPressed() {
+  //   if (_stopWatch.isRunning) {
+  //     _startStopButtonPressed();
+  //   }
+  //   setState(() {
+  //     _stopWatch.reset();
+  //     _setStopwatchText();
+  //   });
+  // }
 
   void _setStopwatchText() {
     _stopwatchText = _stopWatch.elapsed.inHours.toString().padLeft(2, '0') +
@@ -127,7 +148,9 @@ class _CronoPageState extends State<CronoPage> {
           child: Column(
             children: <Widget>[
               // Text(xd, style: TextStyle(fontSize: 25.0, color: Colors.red)),
-              Text(msj, style: TextStyle(fontSize: 25.0, color: Colors.redAccent[700])),
+              Text(msj,
+                  style:
+                      TextStyle(fontSize: 25.0, color: Colors.redAccent[700])),
               // ElevatedButton(
               //   child: Icon( Icons.play_arrow),
               //   onPressed: _startStopButtonPressed,
