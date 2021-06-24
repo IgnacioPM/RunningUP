e760b357d1e311e96f991685c89578a9f7acc59e
IgnacioPM <65568535+IgnacioPM@users.noreply.github.com>
2021-06-24 16:30:10 -0600

    Revert "Merge branch 'oscar' of https://github.com/IgnacioPM/RunningUP into IgnacioPM"
    This reverts commit 3f9bafa1dbc30b1f65fe449c0fc978e6d670d107, reversing
    changes made to d21d8165f136e14d654d02381e85917fdc6c9087.

--- lib/screens/RegistroAct_page.dart
+++ lib/screens/RegistroAct_page.dart
@@ -4,8 +4,7 @@ import 'package:runningup/widgets/drawer.dart';
 
 // import 'package:runningup/widgets/navbar.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
-import 'package:permission_handler/permission_handler.dart';
-import 'package:oktoast/oktoast.dart';
+
 // import 'package:stop_watch_timer/stop_watch_timer.dart';
 class RegisActPage extends StatefulWidget {
   static String id = 'RegisAct_Page';
@@ -17,39 +16,22 @@ class RegisActPage extends StatefulWidget {
 class _RegisActPageState extends State<RegisActPage> {
   String qrValue = "Codigo Qr";
   String msj = '';
-   verificarpermisos_camara() async{
-    var estatusCamara = await Permission.camera.status;
-    var almacen = await Permission.storage.status;
-    print(estatusCamara);
-    print(almacen);
-  //cameraStatus.isGranted == has access to application
-    //cameraStatus.isDenied == does not have access to application, you can request again for the permission. 
-    //cameraStatus.isPermanentlyDenied == does not have access to application, you cannot request again for the permission. 
-    //cameraStatus.isRestricted == because of security/parental control you cannot use this permission. 
-    //cameraStatus.isUndetermined == permission has not asked before. 
-    if(!estatusCamara.isGranted)
-      await Permission.camera.request();
-    if(await Permission.camera.isGranted){
-      scanQr();
-    }else{
-      showToast("Para leer el codigo QR por favor habilita el permiso del uso de camara", position: ToastPosition.bottom);
-    }
-    
-  }
-
-
 
   void scanQr() async {
     String cameraScanResult = await scanner.scan();
     setState(() {
       qrValue = cameraScanResult;
     });
+    // if (cameraScanResult.length > 0) {
+    //         Navigator.pushReplacementNamed(context, '/Crono');
+    //       }
     if (qrValue == 'Iniciar') {
       Navigator.pushReplacementNamed(context, '/Crono');
     }else{
         setState(() {
       msj = 'El codigo QR no es valido, por favor verifique e intente de nuevo.';
     });
+      // final snackBar = SnackBar(content: Text(msj));
     }
   }
 
@@ -57,54 +39,82 @@ class _RegisActPageState extends State<RegisActPage> {
   Widget build(BuildContext context) {
     // var fontweight;
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Registro de Actividad'),
-        ),
-        backgroundColor: MaterialColors.bgColorScreen,
-        drawer: MaterialDrawer(currentPage: "RegisAct_Page"),
-        body: Center(
-          child: Container(
-              child: Text(msj, style: TextStyle(fontSize: 25.0, color: Colors.red)),
-              
-              ),
-        ),
-        floatingActionButton: _crearBotones(),
-        
-        //   floatingActionButton: FloatingActionButton(
-        //     backgroundColor: Colors.green[900],
-        //     onPressed: () =>scanQr(),
-        //     child: Icon(
-        //       Icons.camera_alt_outlined,
-        //     ),
-        //   ),
-        // _cronofloatingActionButton: FloatingActionButton(
-        //     backgroundColor: Colors.red[400],
-        //     onPressed: () {
-        //       Navigator.pushReplacementNamed(context, '/');
-        //     },
-        //     child: Icon(
-        //       Icons.play_circle_fill
-        //     ),
-        //   ),
-          
-        );
-        // ignore: dead_code
-        // Scaffold.of(context).showSnackBar(snackBar);
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Registro de Actividad'),
+      ),
+      backgroundColor: MaterialColors.bgColorScreen,
+      drawer: MaterialDrawer(currentPage: "RegisAct_Page"),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(height: 15.0),
+        ],
+      ),
+    );
   }
 
-  Widget _crearBotones() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        SizedBox(width: 30),
-        FloatingActionButton(
-          backgroundColor: Colors.green[900],
-          onPressed: () => verificarpermisos_camara(),
-          child: Icon(
-            Icons.camera_alt_outlined,
-          ),
+  Widget _cardTipo1() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Card(
+        elevation: 10.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.add_alert_rounded, color: Colors.blue),
+              title: Text('Estas listo para comenzar'),
+              subtitle: Text('Sigue las siguientes instrucciones:'),
+            ),
+            ListTile(
+              subtitle: Text(
+                  '1 - Busca el código  "QR" que se encuentra al inicio de recorrido. \n'),
+            ),
+            ListTile(
+              subtitle: Text(
+                  '2 - Presione el botón "Comenzar" posicionado al final. \n'),
+            ),
+            ListTile(
+              subtitle: Text('3 - Escanea el código "QR". \n'),
+            ),
+            FadeInImage(
+              image: AssetImage('assets/img/qr.jpg'),
+              // image: NetworkImage(
+              //     'http://www.eloriente.net/home/wp-content/uploads/2017/04/ESTADISTICA.jpg'),
+              placeholder: AssetImage('assets/img/cargando.gif'),
+              fadeInDuration: Duration(milliseconds: 200),
+              height: 250.0,
+              fit: BoxFit.cover,
+            ),
+            ListTile(
+             
+              subtitle: Text('4 - El cronometro comenzará automáticamente. \n'),
+            ),
+            ListTile(
+              subtitle: Text('5 - Para finalizar presiona "terminar" y escanea el código "QR" que se encuentra al final del recorrido. \n'),
+            ),
+            Row(  
+              mainAxisAlignment: MainAxisAlignment.center,
+                
+              children: <Widget>[
+                
+                TextButton( 
+                  
+                  child: Text('Comenzar' ,style: TextStyle(color:Colors.white),),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    
+                  
+                  ),
+                  onPressed: () => scanQr(),
+                  
+                ),
+              ],
+            )
+          ],
         ),
       );
     });
